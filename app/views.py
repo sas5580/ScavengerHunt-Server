38e4f038e0d61adbd083867399a9c490a03f3082
Saswata Gupta <saswata.gupta@thalmic.com>
2017-07-04 21:11:51 -0400

    Showed game key on test page

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
-from app import app
+from app import app, Game
 from flask import render_template
 
 @app.route('/')
 def test():
-    return render_template("test.html")
+    return Game.get_by_key(Game.get_list()[0]).key + render_template("test.html")
 
 @app.route('/create')
 @app.route('/index')
