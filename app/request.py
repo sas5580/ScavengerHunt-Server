fa8f37ceeaf9f0bfed3b2388eae17bcc81ae9175
Saswata Gupta <gupta.saswata@gmail.com>
2017-10-10 14:31:45 -0400

    Update request.py

--- app/request.py
+++ app/request.py
@@ -11,9 +11,9 @@ print game1.key
 game1.add_player('Yan', '')
 game1.add_player('Sas', '')
 
-game1.add_objective({"lat": 40.633388, "long": -73.931379}, "NOT CALI OR BUST", "description")
-game1.add_objective({"lat": 40.632397, "long": -73.931349}, "YAN IS GAY", "description2")
-game1.add_objective({"lat": 40.632377, "long": -73.931364}, "MEMES", "description2")
+game1.add_objective({"lat": 40.633388, "long": -73.931379}, "TEST_OBJECTIVE_1", "description1")
+game1.add_objective({"lat": 40.632397, "long": -73.931349}, "TEST_OBJECTIVE_2", "description2")
+game1.add_objective({"lat": 40.632377, "long": -73.931364}, "TEST_OBJECTIVE_3", "description3")
 
 for o in game1.objective_list():
     print o.id
